a09ca9dafcc82442c80328668260e4d5974b3165
unknown <dominik.kis.njemacki@gmail.com>
2020-04-07 19:33:32 +0200

    moj prvi commit

---
+++ SpaDomacaZadaca01/Source.cpp
@@ -0,0 +1,28 @@
+#include <SFML/Graphics.hpp>
+#include "Cvijet.h"
+
+int main()
+{
+	sf::ContextSettings settings;
+	settings.antialiasingLevel = 8;
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Hello, SFML world!", sf::Style::Default, settings);
+	window.setFramerateLimit(60);
+	Cvijet cvijet(&window);
+	int brojilo = 0;
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+
+		window.clear();
+		cvijet.draw(brojilo);
+		window.display();
+	}
+
+	return 0;
+}
